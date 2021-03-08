1d32d5c4468e61836193cd3a7b188d534e175897
NatalliaAkaronka <n.g.okoronko@mail.ru>
2021-03-08 16:36:33 +0300

    Добавьте файлы проекта.

---
+++ Lab05_Okoronko/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05_Okoronko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Циклы с контролем за монотонной велечиной";
+            Console.Write("Введите переменную х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите точность: ");
+            double точность = Convert.ToDouble(Console.ReadLine());
+            double a = x;
+            double a1 = 0;
+            while ( Math.Abs(a1-a)> точность)
+            {
+                a1 = a;
+                a=Math.Pow((Math.Abs(4 * Math.Pow(a, 2) - 2 * x)), 1.0/3.0);  // При заданном условии задачи функция расходящаяся и стремится к бесконечности, что не дают правильно определить первый член последовательности. Поэтому сделала сходящуюся функцию, заменив корень квадратный на корень 3 степени.
+            }
+
+            string txt = "Первое число последновательности равно: ";
+            Console.WriteLine(txt + a);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Lab05_Okoronko/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab05_Okoronko")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab05_Okoronko")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ef0ad946-f555-43db-9fb6-c1e70e3a25da")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
